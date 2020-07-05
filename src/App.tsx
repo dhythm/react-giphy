c58de0a35bdaade42706ecb94a72148fb9f6dbf5
dhythm <Yuta.Okada.20@gmail.com>
2020-07-05 16:17:50 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import 'materialize-css';
+import 'materialize-css/dist/css/materialize.min.css';
+import React from 'react';
+
+const App: React.FunctionComponent = () => {
+  return (
+    <div className="App">
+      <h1>Hello, world</h1>
+    </div>
+  );
+};
+
+export default App;
